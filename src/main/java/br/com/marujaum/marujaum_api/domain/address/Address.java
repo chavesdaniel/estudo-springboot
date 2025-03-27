ebe4f93058d0c001ea7b4070aa199ccb31fa9964
Daniel Chaves <chavesdaniel@gmail.com>
2025-03-27 07:57:22 -0300

    feat: mapeamento de entidades

---
+++ src/main/java/br/com/marujaum/marujaum_api/domain/address/Address.java
@@ -0,0 +1,21 @@
+package br.com.marujaum.marujaum_api.domain.address;
+
+import br.com.marujaum.marujaum_api.domain.event.Event;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Table(name="address")
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name="event_id")
+    private Event event;
+}
--- src/main/java/br/com/marujaum/marujaum_api/domain/coupon/Coupon.java
+++ src/main/java/br/com/marujaum/marujaum_api/domain/coupon/Coupon.java
@@ -1,14 +1,13 @@
 package br.com.marujaum.marujaum_api.domain.coupon;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import br.com.marujaum.marujaum_api.domain.event.Event;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
@@ -21,4 +20,12 @@ public class Coupon {
     @Id
     @GeneratedValue
     private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name="event_id")
+    private Event event;
 }
